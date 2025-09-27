f3cc997ef23b0df6d053571a1528a121f6fcf93d
Isaac McMullen <ihmcmull@uark.edu>
2025-09-27 13:09:31 -0500

    SQL Ready for testing, removed casting of int to string

--- game.py
+++ game.py
@@ -150,8 +150,7 @@ class Model():
 		self.temp_id = id
 		# Check if id is in database; if in, set temp_code_name to code name in database
 		sql_query = "SELECT * FROM players WHERE id LIKE %s;"
-		id_str = str(id)
-		self.cursor.execute(sql_query, (id_str,))
+		self.cursor.execute(sql_query, (id,))
 		rows = cursor.fetchall()
 		# Set this to failure and then change it if we succeed
 		self.need_code_name = True
@@ -166,8 +165,7 @@ class Model():
 	def enter_code_name(self, code_name):
 #		# Enter id and code name into database
 		sql_query = "INSERT INTO players (id, codename) VALUES (%s, %s);"
-		id_str = str(self.temp_id)
-		self.cursor.execute(sql_query,(id_str, code_name))
+		self.cursor.execute(sql_query,(id, code_name))
 		self.cursor.commit()
 		# create temporary code name
 		self.temp_code_name = code_name
