a45e9ed5398adaee1f002c3c0998393bd9782b96
louistorabi <ltorabi@uark.edu>
2025-11-09 16:56:32 -0600

    Update print statement to say 'Goodbye World'

--- game.py
+++ game.py
@@ -116,7 +116,7 @@ class Model():
 		self.audio_started = False
 		self.audio_start_at = 14.8
 		track_num = random.randint(1,8)
-		self.audio_file = "sounds/Track"{track_num:02d}".mp3"
+		self.audio_file = f"sounds/Track{track_num:02d}.mp3"
 		self.audio_volume = 0.8
 
 		# Create Highest Scorer, for the player with the most points at any given time, updated when an event happens (ie, someone tags someone)
@@ -1013,6 +1013,7 @@ while c.keep_going:
 	sleep(sleep_time)
 m.conn.close()
 m.cursor.close()
+
 
 
 
